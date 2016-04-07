0a9395b471eef8ddfdd03d6361d1efcd98eb4ce3
Stanislav Varenkov <s.varenkov@splesk.com>
2016-04-07 18:29:44 +0300

    Draft for detection external client IP

--- index.php
+++ index.php
@@ -19,8 +19,40 @@ $app->get('/', function(Request $request) use ($app) {
 $app->run();
 
 
+/**
+ * @param Request $request
+ * @return string
+ *
+ * Есть RFC для Forwarded HTTP, заголовок Forwarded: for <ip>
+ * @link https://tools.ietf.org/html/rfc7239
+ * @link http://stackoverflow.com/questions/7445592/what-is-the-difference-between-http-client-ip-and-http-x-forwarded-for
+ *
+ * Можно определять внешний IP-адрес клиента с помощью нестандартизированных заголовков:
+ * X-Forwarded-For, X-Client-IP, X-Real-IP
+ * @link http://stackoverflow.com/questions/527638/getting-the-client-ip-address-remote-addr-http-x-forwarded-for-what-else-coul
+ */
 function getClientIP(Request$request) {
-    return "127.0.0.1";
+    $ipValidatorOptions = [FILTER_FLAG_IPV4, FILTER_FLAG_IPV6, FILTER_FLAG_NO_PRIV_RANGE, FILTER_FLAG_NO_RES_RANGE];
+
+    $ip = $request->headers->get('X-Client-IP');
+    if (filter_var($ip, FILTER_VALIDATE_IP, $ipValidatorOptions) !== false) {
+        return $ip;
+    }
+
+    $ip = $request->headers->get('X-Real-IP');
+    if (filter_var($ip, FILTER_VALIDATE_IP, $ipValidatorOptions) !== false) {
+        return $ip;
+    }
+
+    // @todo: вообще-то, нужно обработать случай с несколькими IP
+    $ip = $request->headers->get('X-Forwarded-For');
+    if (filter_var($ip, FILTER_VALIDATE_IP, $ipValidatorOptions) !== false) {
+        return $ip;
+    }
+
+    // -- О ужас! Ничего не получилось,мы все пропали!
+    // -- Помолимся братья и сёстры нашему древнему богу REMOTE_ADDR!
+    return $_SERVER['REMOTE_ADDR'];
 }
 
 /**
@@ -34,3 +66,5 @@ function getLocation($clientIp) {
         'lon' => 666
     ];
 }
+
+

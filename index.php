6adbb93684800499f30192498f056e0a8d2dd4a9
Stanislav Varenkov <s.varenkov@splesk.com>
2016-04-07 19:18:29 +0300

    Draft for Yandex API Locator Request

--- index.php
+++ index.php
@@ -6,14 +6,18 @@ require_once ("vendor/autoload.php");
 use \Symfony\Component\HttpFoundation\Request;
 use \Silex\Application;
 
-use GuzzleHttp\Psr7\Request as GuzzleRequest;
-
 $app = new Application();
+//$app['debug'] = true;
 
 $app->get('/', function(Request $request) use ($app) {
-    $clientIP = getClientIP($request);
-    $location = getLocation($clientIP);
-    return $app->json($location);
+    try {
+        $clientIP = getClientIP($request);
+        $location = getLocation($clientIP);
+        $response = $app->json($location);
+    } catch (Exception $e) {
+        $response = $e->getMessage();
+    }
+    return $response;
 });
 
 $app->run();
@@ -32,7 +36,7 @@ $app->run();
  * @link http://stackoverflow.com/questions/527638/getting-the-client-ip-address-remote-addr-http-x-forwarded-for-what-else-coul
  */
 function getClientIP(Request$request) {
-    $ipValidatorOptions = [FILTER_FLAG_IPV4, FILTER_FLAG_IPV6, FILTER_FLAG_NO_PRIV_RANGE, FILTER_FLAG_NO_RES_RANGE];
+    $ipValidatorOptions = [FILTER_FLAG_IPV4, FILTER_FLAG_NO_PRIV_RANGE, FILTER_FLAG_NO_RES_RANGE];
 
     $ip = $request->headers->get('X-Client-IP');
     if (filter_var($ip, FILTER_VALIDATE_IP, $ipValidatorOptions) !== false) {
@@ -55,15 +59,42 @@ function getClientIP(Request$request) {
     return $_SERVER['REMOTE_ADDR'];
 }
 
+use GuzzleHttp\Client;
 /**
  * @param string $clientIp
  * @return array
+ * @throws Exception
  */
 function getLocation($clientIp) {
-    //$guzzleRequest = new GuzzleRequest();
+    $client = new Client([
+        'base_uri' => 'http://api.lbs.yandex.net'
+    ]);
+    $jsonData = new stdClass();
+    $jsonData->common = new stdClass();
+    $jsonData->common->version = '1.0';
+    $jsonData->common->api_key = YANDEX_LOCATOR_KEY;
+    $jsonData->ip = new stdClass();
+    $jsonData->ip->address_v4 = $clientIp;
+    $response = $client->post('/geolocation',[
+        'form_params' => [
+            'json' => json_encode($jsonData)
+        ]
+    ]);
+    $code = $response->getStatusCode();
+    if ($code != 200) {
+        // @todo: ошибки должны быть более специфичными
+        throw new Exception('Беда, насалькика!' . PHP_EOL . $response->getReasonPhrase());
+    }
+    $location = json_decode($response->getBody()->getContents());
+    if (!empty($location->error)) {
+        // @todo: ошибки должны быть более специфичными
+        throw new Exception('Беда, насалькика!' . PHP_EOL . $location->error);
+    }
+
     return [
-        'lat' => 666,
-        'lon' => 666
+        'lat' => $location->position->latitude,
+        'lon' => $location->position->longitude,
+        'additional_message' => $location->position->precision >= 100000 ? 'Reliably locate failed' : ''
     ];
 }
 
